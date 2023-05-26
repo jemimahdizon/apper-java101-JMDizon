d9f79ef1716b2890883599a82cfb1dc783db62e6
jemimahdizon <130464986+jemimahdizon@users.noreply.github.com>
2023-05-26 10:02:57 +0800

    added nextLine
    so it exits when an empty line is inputted

--- finalProject/gcash/src/shareLoad.java
+++ finalProject/gcash/src/shareLoad.java
@@ -4,20 +4,20 @@ import java.util.Scanner;
 public class shareLoad {
     public static void shareLoad(Scanner scanner, Map<String, Double> balances, Map<String, String> users) {
         System.out.print("Enter sender's mobile number: ");
-        String sender = scanner.next();
+        String sender = scanner.nextLine().trim();
 
-        // Check if the sender's mobile number is registered
-        if (!users.containsKey(sender)) {
-            System.out.println("Sender's mobile number is not registered!");
+        // Check if the sender's mobile number is empty or not registered
+        if (sender.isEmpty() || !users.containsKey(sender)) {
+            System.out.println("Sender's mobile number is not valid or not registered!");
             return;
         }
 
         System.out.print("Enter recipient's mobile number: ");
-        String recipient = scanner.next();
+        String recipient = scanner.nextLine().trim();
 
-        // Check if the recipient's mobile number is registered
-        if (!users.containsKey(recipient)) {
-            System.out.println("Recipient's mobile number is not registered!");
+        // Check if the recipient's mobile number is empty or not registered
+        if (recipient.isEmpty() || !users.containsKey(recipient)) {
+            System.out.println("Recipient's mobile number is not valid or not registered!");
             return;
         }
 
@@ -28,7 +28,15 @@ public class shareLoad {
         }
 
         System.out.print("Enter amount to share: ");
-        double amount = scanner.nextDouble();
+        double amount;
+
+        // Check if the amount input is valid
+        if (scanner.hasNextDouble()) {
+            amount = scanner.nextDouble();
+        } else {
+            System.out.println("Invalid amount!");
+            return;
+        }
 
         // Check if the amount is valid
         if (amount <= 0) {
